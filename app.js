ddba6605a92123c5d4a3b611a8e3470aa8a712c0
Mahfuzur Rahman <mrahman516@gmaile.com>
2021-02-09 08:05:55 +0600

    2nd commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-.then(res => res.json())
-.then(data => displayFoods(data))
+// fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+// .then(res => res.json())
+// .then(data => displayFoods(data))
 
 
-const displayFoods = foods =>{
-    for (let i = 0; i < foods.length; i++) {
-        const food = foods[i];
-        console.log(food.name);
+// const displayFoods = foods =>{
+//     for (let i = 0; i < foods.length; i++) {
+//         const food = foods[i];
+//         console.log(food.name);
         
-    }
-}
+//     }
+// }
